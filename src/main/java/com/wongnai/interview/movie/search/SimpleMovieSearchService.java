bcd62409dd68ec00fe88780ea5ae3ed95bff1dd3
yaipajim <60070066@kmitl.ac.th>
2019-04-17 18:30:39 +0700

    Add algorithm search

--- src/main/java/com/wongnai/interview/movie/search/SimpleMovieSearchService.java
+++ src/main/java/com/wongnai/interview/movie/search/SimpleMovieSearchService.java
@@ -1,7 +1,9 @@
 package com.wongnai.interview.movie.search;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import com.wongnai.interview.movie.external.MovieData;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -19,6 +21,19 @@ public class SimpleMovieSearchService implements MovieSearchService {
 		//TODO: Step 2 => Implement this method by using data from MovieDataService
 		// All test in SimpleMovieSearchServiceIntegrationTest must pass.
 		// Please do not change @Component annotation on this class
-		return null;
+
+        //init return value
+        ArrayList<Movie> movieResult  = new ArrayList<>();
+		//search algorithm
+		for (MovieData movieData : movieDataService.fetchAll()){
+		    String[] titleSplit = movieData.getTitle().split(" ");
+		    // for in titlesplit
+            for(String splitText : titleSplit){
+                if(splitText.equals(queryText)){
+                    System.out.println(movieData.getTitle());
+                }
+            }
+        }
+		return movieResult;
 	}
 }
