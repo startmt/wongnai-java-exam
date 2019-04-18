ff230cfd1beb35d412c605c08c017867c3a712cc
startmt <59070038@it.kmitl.ac.th>
2019-04-18 15:17:45 +0700

    init in memory db

--- src/main/java/com/wongnai/interview/config/MovieConfiguration.java
+++ src/main/java/com/wongnai/interview/config/MovieConfiguration.java
@@ -4,10 +4,20 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.HashMap;
+import java.util.TreeMap;
+
 @Configuration
 public class MovieConfiguration {
 	@Bean
 	public RestTemplate restTemplate() {
 		return new RestTemplate();
 	}
+
+	@Bean("invertedIndexDatabase")
+    public TreeMap invertedIndexDatabase(){
+        TreeMap<String, Integer> wordMap = new TreeMap<String, Integer>( );
+
+        return  wordMap;
+    }
 }
